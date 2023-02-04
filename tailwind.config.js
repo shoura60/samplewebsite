2b0f823fa37775e26f0788ad7ec49b1ab7e8d3c2
Shoura Rahmanizadeh <123620559+shoura60@users.noreply.github.com>
2023-02-04 15:35:50 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontfamily: {
+       poppins:["poppins, sans-serif"],
+    },
+  },
+  plugins: [],
+}
